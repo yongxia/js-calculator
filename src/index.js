12b4e4a538947fddaddacd764592f6cf684d248a
Yong Xia <xywork008@gmail.com>
2021-05-09 15:33:22 -0500

    update layout for calculator

--- src/index.js
+++ src/index.js
@@ -6,22 +6,40 @@ import './index.css';
 class Presentational extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      display: 0
+    }
   }
   render() {
 
     return (
       <div className='number-group'>
+        <div id="box">
+          <div id="display">{this.state.display}</div>
+          <div id="formula"></div>
+        </div>
+        <button id="clear">AC</button>
+        <button id="divide">/</button>
+        <button id="multiply">*</button>
+
         <button id="seven">7</button>
         <button id="eight">8</button>
         <button id="nine">9</button>
+        <button id="add">+</button>
+
         <button id="four">4</button>
         <button id="five">5</button>
         <button id="six">6</button>
+        <button id="subtract">-</button>
+
         <button id="one">1</button>
         <button id="two">2</button>
         <button id="three">3</button>
+
+        <button id="equals">=</button>
         <button id="zero">0</button>
-        <button id="dot">.</button>
+        <button id="decimal">.</button>
+
       </div>
     )
   }
