377c3a03eaf2f9b0b2534e55f5f9df322dd4eb3b
Yong Xia <xywork008@gmail.com>
2021-05-10 15:19:11 -0500

    add handleClick and handle operation(only keep the last operation)

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const clear = () => ({
 });
 
 const INTIAL_STATE = ({
-  messages: [],
+  messages: '',
   result: 0,
   reset: false
 });
@@ -32,7 +32,10 @@ const INTIAL_STATE = ({
 const calculatorReducer = (state = INTIAL_STATE, action) => {
   switch (action.type) {
     case MESSAGES:
-      return { ...state, messages: [...state.messages, action.message] };
+      let messages = state.messages + action.message;
+      let reOperation = /[\+\-\*\/]+$/;
+      messages = messages.replace(reOperation, messages.charAt(messages.length - 1));
+      return { ...state, messages: messages };
     case RESULT:
       let result = 1;
       return { ...state, result: result };
@@ -63,37 +66,57 @@ const mapDispatchToProps = dispatch => {
 class Presentational extends React.Component {
   constructor(props) {
     super(props);
+    this.resetHandler = this.resetHandler.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  resetHandler = () => {
+    this.props.reset();
+  }
+
+  clickHandler = (e) => {
+    this.props.addNewMessage(e.target.value);
+
+
+    //let curMessages = [this.props.messages.split(""), e.target.value].join("");
+    //console.log(curMessages, /\*[+\-\*/]{2}$/.match(curMessages))
+    //only keep last operaion except for - for negative number
+    //curMessages = curMessages.replace(/\*[+-\*/]{2}$/, '$1', 'hi');
+
   }
 
   render() {
 
     return (
-      <div className='number-group'>
-        <div id="box">
-          <div id="formula">{this.props.messages.join("")}</div>
-          <div id="display">{this.props.result}</div>
+      <div className="container">
+        <div className='box-group'>
+          <div className="screen">
+            <div id="formula">{this.props.messages}</div>
+            <div id="display">{this.props.result}</div>
+          </div>
+          <button id="clear" onClick={this.resetHandler}>AC</button>
+          <button id="divide" onClick={this.clickHandler} value="/">/</button>
+          <button id="multiply" onClick={this.clickHandler} value="*">*</button>
+
+          <button id="seven" onClick={this.clickHandler} value="7">7</button>
+          <button id="eight" onClick={this.clickHandler} value="8">8</button>
+          <button id="nine" onClick={this.clickHandler} value="9">9</button>
+          <button id="add" onClick={this.clickHandler} value="+">+</button>
+
+          <button id="four" onClick={this.clickHandler} value="4">4</button>
+          <button id="five" onClick={this.clickHandler} value="5">5</button>
+          <button id="six" onClick={this.clickHandler} value="6">6</button>
+          <button id="subtract" onClick={this.clickHandler} value="-">-</button>
+
+          <button id="one" onClick={this.clickHandler} value="1">1</button>
+          <button id="two" onClick={this.clickHandler} value="2">2</button>
+          <button id="three" onClick={this.clickHandler} value="3">3</button>
+
+          <button id="equals" onClick={this.clickHandler}>=</button>
+          <button id="zero" onClick={this.clickHandler} value="0">0</button>
+          <button id="decimal" onClick={this.clickHandler} value=".">.</button>
         </div>
-        <button id="clear">AC</button>
-        <button id="divide">/</button>
-        <button id="multiply">*</button>
-
-        <button id="seven">7</button>
-        <button id="eight">8</button>
-        <button id="nine">9</button>
-        <button id="add">+</button>
-
-        <button id="four">4</button>
-        <button id="five">5</button>
-        <button id="six">6</button>
-        <button id="subtract">-</button>
-
-        <button id="one">1</button>
-        <button id="two">2</button>
-        <button id="three">3</button>
-
-        <button id="equals">=</button>
-        <button id="zero">0</button>
-        <button id="decimal">.</button>
+        <div><p>Coded by Yong Xia</p></div>
       </div>
     )
   }
