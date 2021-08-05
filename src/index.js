221b3bee83860637b7795148ae1d012ed945e1cb
Yong Xia <xywork008@gmail.com>
2021-08-05 15:37:55 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import { connect, Provider } from 'react-redux';
+
 import './style.css';
-import { connect, Provider } from 'react-redux'
 
 //Redux
 const MESSAGES = 'MESSAGES',
@@ -25,26 +26,23 @@ const clear = () => ({
 });
 
 const INTIAL_STATE = ({
-  messages: [], // store operators and numbers
+  equation: [], // store operators and numbers
   result: 0,
-  parser: '' //--123+ parse to messages:['-', '-123'], parser: '+'
+  parser: ''
 });
 
 
 const calculatorReducer = (state = INTIAL_STATE, action) => {
-  let messages = [...state.messages];
+  let equation = [...state.equation];
   let parser = state.parser;
   switch (action.type) {
     case MESSAGES:
-      if (messages.includes('=')) {
-        if (/[+*/-]/.test(action.message)) { //user previous result
-          messages = [state.result];
-        } else {
-          messages = []; //new calculation
-        }
-      }
       parser += action.message;
-      //reset to last operator (case: +/* followed by any operator, or - followed by +*/)
+      //case: use previous result
+      if (equation.includes('=')) {
+        equation = /[+*/-]/.test(action.message) ? [state.result] : [];
+      }
+      //case: reset to last operator (+/-* followed by +/*)
       if (/^[+/*-][+/*]/.test(parser)) {
         parser = parser.substr(1);
         //reset to last operator (case: -- foloweed by any operator)
@@ -65,43 +63,46 @@ const calculatorReducer = (state = INTIAL_STATE, action) => {
         //meet condition for parsing, ends with at least one digit and an operator
         //at the very beginng, there might not be an operator in parser
         if (/^[+/*-]/.test(parser)) {
-          messages.push(parser.charAt(0));
+          equation.push(parser.charAt(0));
           parser = parser.substring(1); //consume operator, '--1.23+' => '-1.23+' , messages:['-']
         }
         //continue consume parser number part, messages: ['-',-1.23], parser: '+'
         if (parser.includes('.')) {
-          messages.push(parseFloat(parser.substring(0, parser.length - 1)));
+          equation.push(parseFloat(parser.substring(0, parser.length - 1)));
         } else {
-          messages.push(parseInt(parser.substring(0, parser.length - 1)));
+          equation.push(parseInt(parser.substring(0, parser.length - 1)));
         }
         //update parser for next parse
         parser = parser.substring(parser.length - 1);
       }
-      return { ...state, messages: messages, parser: parser, result: parser };
+      return { ...state, equation: equation, parser: parser, result: parser };
     case RESULT:
       //consume remaining parser if any
       if (/^[+*/-]/.test(parser)) {
-        messages.push(parser.charAt(0));
+        equation.push(parser.charAt(0));
         parser = parser.substring(1);
         if (parser.length > 0) {
           if (parser.includes('.')) {
-            messages.push(parseFloat(parser));
+            equation.push(parseFloat(parser));
           } else {
-            messages.push(parseInt(parser));
+            equation.push(parseInt(parser));
           }
           parser = '';
         }
       }
-      // if there is no left hand for first operator in array, use 0
-      if (typeof messages[0] !== 'number') {
-        messages.unshift(0);
-      }
       // no right hand number return current state
-      if (messages.length <= 2 || typeof messages[messages.length - 1] !== 'number') return state;
-      const expression = messages.join(' ');
-      let result = eval(expression);
-      messages.push('=', result);
-      return { ...state, messages: messages, parser: '', result: result };
+      if (equation.length <= 2 || typeof equation[equation.length - 1] !== 'number') return state;
+
+      let expression = equation.join(' ');
+
+      // no left hand for first operator in expression, add 0
+      if (typeof equation[0] !== 'number') {
+        expression = '0 + ' + expression;
+      }
+
+      let result = Math.round(eval(expression) * 10000) / 10000;
+      equation.push('=', result);
+      return { ...state, equation: equation, parser: '', result: result };
     case CLEAR:
       return INTIAL_STATE;
     default:
@@ -111,7 +112,7 @@ const calculatorReducer = (state = INTIAL_STATE, action) => {
 
 const mapStateToProps = state => {
   return {
-    messages: state.messages,
+    equation: state.equation,
     result: state.result,
     parser: state.parser
   }
@@ -152,7 +153,7 @@ class Presentational extends React.Component {
       <div className="container">
         <div className='box-group'>
           <div className="screen">
-            <div id="formula">{this.props.messages.join("") + this.props.parser}</div>
+            <div id="formula">{this.props.equation.join('') + this.props.parser}</div>
             <div id="display">{this.props.result}</div>
           </div>
           <button id="clear" onClick={this.resetHandler}>AC</button>
